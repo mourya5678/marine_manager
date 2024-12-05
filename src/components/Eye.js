f111051b1fc8349de6b86d1e17df97368569fae8
mourya5678 <abhay.ctinfotech@gmail.com>
2024-12-05 16:19:46 +0530

    new_changes

--- src/components/Eye.js
+++ src/components/Eye.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Eye = ({ isEye, onClick }) => {
     return (
         <div>
-            <i className={`fa-regular ${!isEye ? 'fa-eye-slash' : 'fa-eye'} ct_hide_pass_wye`} onClick={onClick}></i>
+            <i className={`fa-regular ab_pointer ${!isEye ? 'fa-eye-slash' : 'fa-eye'} ct_hide_pass_wye`} onClick={onClick}></i>
         </div>
     )
 }
