8a3b10379ffc8e66721fea38c1a6bdd7d96b3403
mourya5678 <abhay.ctinfotech@gmail.com>
2025-02-14 16:39:22 +0530

    new_changes

--- src/routes/BackendRoutes.js
+++ src/routes/BackendRoutes.js
@@ -5,11 +5,13 @@ export const BASE_URL = "http://3.26.177.93:4000/";   // Live Url
 // auth user
 export const signupEndPointURL = "users/signup";
 export const loginEndPointURL = "users/login";
+
 export const forgotPasswordEndPointURL = "users/forgetPassword";
 
 // Dashboard Data
 export const getDashboardDataEndPointURL = "home/getHome";
 export const getTomorrowTaskEndPointURL = "home/tasksForTommorrow";
+
 export const getStaffTaskEndPointURL = "home/staffMembers";
 
 // Bussiness profile
@@ -19,25 +21,29 @@ export const updateBussinessProfileEndPointURL = "users/editProfile";
 // staff 
 export const addStaffEndPointURL = "staff/addStaffMember";
 export const getStaffDataEndPointURL = "staff/staffMembers";
+
 export const updateStaffEndPointURL = "staff/editStaffMember";
 export const getActiveStaffDataEndPointURL = "staff/activeStaffMembers";
 
 // supplier
 export const getSupplierEndPointURL = "supplier/getAllSupplier";
 export const addSupplierEndPointURL = "supplier/addsupllier";
+
 export const updateSupplierEndPointURL = "supplier/editsupllier";
 
 // Dock 
 export const addDockDetailsEndPointURL = "dock/addDock";
 export const getDockDetailsEndPointURL = "dock/getAllDock";
+
 export const updateDockDetailsEndPointURL = "dock/updateDock";
 export const getAvailableBoatsEndPointURL = "dock/availableBoats";
-export const assignBoatsToDockEndPointURL = "dock/assignBoat";
 
+export const assignBoatsToDockEndPointURL = "dock/assignBoat";
 
 // Boats
 export const getAllBoatsDataEndPointURL = "boat/getAllBoat";
 export const addBoatDetailsEndPointURL = "boat/addBoat";
+
 export const updateBoatDetailsEndPointURL = "boat/updateBoat"
 
 // Images Delete 
@@ -46,14 +52,17 @@ export const deleteImageEndPointURL = "users/deleteFile";
 // Task
 export const createTaskEndPointURL = "task/addTask";
 export const getAllBoatWithTasksEndPointURL = "task/getAllBoatsWithTasks";
+
 export const updateTaskEndPointURL = "task/updateTask";
 export const getAllTaskDataEndPointURL = "task/getAllTask";
 
 // Lead
 export const getAllLeaddataEndPointURL = "quickLeads/getAllQuickLeads";
 export const AddLeaddataEndPointURL = "quickLeads/addQuickLeads";
+
 export const UpdateLeaddataEndPointURL = "quickLeads/updateQuickLeads";
 export const RecouringReminderEndPointURL = "quickLeads/getDueRecurringTasks";
+
 export const UpdateRecouringEndPointURL = "quickLeads/updateRecurringTask";
 
 // Notification
